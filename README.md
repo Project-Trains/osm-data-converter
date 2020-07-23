979f9f6efd78a4269fe55f6406c3469b757fd6f1
kalucky0 <kalucki23@gmail.com>
2020-07-23 09:33:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# OSM data converter
+Data converter for OSM SHP files. Converts from SHP, DBF and PRJ files to JSON and CSV. Made for Project Trains.
+
+### Contributing
+Thanks for thinking of contributing to this project. smile
+
+Before sending a Pull Request, please make sure that you're assigned the task on a GitHub issue.
+
+* If a relevant issue already exists, discuss on the issue and get it assigned to yourself on GitHub.
+* If no relevant issue exists, open a new issue and get it assigned to yourself on GitHub.
+
+Please proceed with a Pull Request only after you're assigned. It'd be sad if your Pull Request (and your hardwork) isn't accepted just because it isn't ideologically compatible.
+
+### License
+You are free to:
+* Share — copy and redistribute the material in any medium or format
+* Adapt — remix, transform, and build upon the material
+  for any purpose, even commercially.
+  
+Under a Creative Commons Attribution 4.0 International License.
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2020 kalucky0
+ * This file is part of Project Trains OSM Data Converter.
+ *
+ * OSM Data Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the Creative Commons Attribution 4.0 International License.
+ */
+
+const shp = require('shp');
+const fs = require('fs');
+
+// shpToJson('data/gis_osm_railways_free_1');
+// shpToJson('data/gis_osm_buildings_a_free_1');
+
+function shpToJson(input) {
+    let j = 0;
+    console.log("Parsing file...");
+    const json = JSON.parse(JSON.stringify(shp.readFileSync(input)))["features"];
+    console.log("Length: ", json.length);
+    let all = [];
+    for (let i = 0; i < json.length; i++) {
+        let properties = {};
+        for (let key in json[i].properties) {
+            properties[key] = parseInt(json[i].properties[key].replace(/^[ ]+|[ ]+$/g, ""))
+            if (isNaN(properties[key]))
+                properties[key] = json[i].properties[key].replace(/^[ ]+|[ ]+$/g, "")
+            if (properties[key] === "")
+                properties[key] = null
+        }
+        properties.geometry = json[i].geometry;
+        all.push(properties);
+        if (i % 10000 === 0 && i > 1) {
+            j++;
+            fs.writeFileSync("output/json/data" + j + ".json", JSON.stringify(all));
+            console.log("File: ", j);
+            all = [];
+        }
+    }
+    j++;
+    fs.writeFileSync("output/json/data" + j + ".json", JSON.stringify(all));
+    all = [];
+}
+
+function jsonToCsv(input) {
+    //Doesn't work yet
+    const data = JSON.parse(fs.readFileSync(input));
+
+    let j = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (i % 4000 === 0) j++;
+        fs.appendFileSync("output/csv/data" + j + ".csv", `"${i + 1}","${JSON.stringify(data[i].geometry.coordinates)}","${data[i].properties.fclass}",${(data[i].properties.name == "" ? "NULL" : `${JSON.stringify(data[i].properties.name.replace(/\"/gi, ''))}`)},"${(data[i].properties.bridge == "T" ? 1 : 0)}","${(data[i].properties.tunnel == "T" ? 1 : 0)}"\r\n`)
+    }
+}
